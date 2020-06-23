5e3d9947b3fac3b314fe2f38afd1deec41dbb6ba
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-06-22 22:12:00 -0300

    [flyway] mysql db migrações com Flyway

---
+++ os-api/src/main/resources/db/migration/V001__create-table-client.sql
@@ -0,0 +1,9 @@
+create table cliente (
+	id bigint not null auto_increment,
+	nome varchar(60) not null,
+	email varchar(255) not null,
+	phone varchar(20) not null,
+	endereco varchar(255) not null,
+	
+	primary key (id)
+);
