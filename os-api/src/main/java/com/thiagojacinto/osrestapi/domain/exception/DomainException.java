262301b197ec6450fdcc64880757d526f087fe9b
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-06-22 22:16:27 -0300

    [exception] personalização das exceções da camada de negócio

---
+++ os-api/src/main/java/com/thiagojacinto/osrestapi/domain/exception/DomainException.java
@@ -0,0 +1,11 @@
+package com.thiagojacinto.osrestapi.domain.exception;
+
+public class DomainException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public DomainException(String message) {
+		super(message);
+	}
+	
+}
---
+++ os-api/src/main/java/com/thiagojacinto/osrestapi/domain/exception/EntityNotFoundException.java
@@ -0,0 +1,13 @@
+package com.thiagojacinto.osrestapi.domain.exception;
+
+public class EntityNotFoundException extends DomainException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public EntityNotFoundException(String message) {
+		
+		super(message);
+		
+	}
+
+}
