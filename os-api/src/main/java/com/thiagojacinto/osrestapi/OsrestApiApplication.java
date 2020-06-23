93e32b4ca0e97c04e3407a3336344048479df73c
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-06-22 22:08:42 -0300

    [init] projeto spring arquivos iniciais + pom.xml do MAVEN

---
+++ os-api/src/main/java/com/thiagojacinto/osrestapi/OsrestApiApplication.java
@@ -0,0 +1,13 @@
+package com.thiagojacinto.osrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsrestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsrestApiApplication.class, args);
+	}
+
+}
---
+++ os-api/src/test/java/com/thiagojacinto/osrestapi/OsrestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagojacinto.osrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsrestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
